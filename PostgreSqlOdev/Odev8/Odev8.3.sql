c2536d303a2193de576ab30ef90ac86fbe42d81a
Ömer <okozluca.1@gmail.com>
2024-11-07 16:31:09 +0300

    Add files via upload

---
+++ PostgreSqlOdev/Odev8/Odev8.3.sql
@@ -0,0 +1,9 @@
+UPDATE employee
+SET 
+name = 'Ahmet Haydar',
+birthday ='2012-06-12',
+email='Ahmethaydar@hotmail.com'
+
+WHERE id >45;
+
+--Sütunların her birine göre diğer sütunları güncelleyecek 5 adet UPDATE işlemi yapalım.
---
+++ PostgreSqlOdev/Odev8/Odev8.4.sql
@@ -0,0 +1,4 @@
+DELETE  FROM employee
+WHERE id >45;
+
+--Sütunların her birine göre ilgili satırı silecek 5 adet DELETE işlemi yapalım.
