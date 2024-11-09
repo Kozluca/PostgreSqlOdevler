ac29428a869a9e6acfb278a6685d0f0c30517c29
Ömer <okozluca.1@gmail.com>
2024-11-09 23:25:35 +0300

    Add files via upload

---
+++ PostgreSqlOdev/Odev9/Odev 9.3.sql
@@ -0,0 +1,4 @@
+SELECT rental.rental_id , customer.first_name, customer.last_name FROM customer
+INNER JOIN rental ON rental.rental_id= customer.customer_id;
+--customer tablosu ile rental tablosunda bulunan rental_id ile 
+--customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
