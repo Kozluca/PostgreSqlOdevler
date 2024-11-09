2076cbd13aa35aef062fbf19691f4f44a3048662
Ömer <okozluca.1@gmail.com>
2024-11-09 23:25:01 +0300

    Delete Odev10 directory

--- Odev10/Odev 10.2.sql
+++
@@ -1,5 +0,0 @@
-SELECT customer.first_name , customer.last_name , payment.payment_id FROM customer
-RIGHT JOIN payment ON payment.payment_id=customer.customer_id;
-
---customer tablosu ile payment tablosunda bulunan payment_id ile 
---customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz RIGHT JOIN sorgusunu yazınız.
