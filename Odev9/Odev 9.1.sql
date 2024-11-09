044ffad6494c2a81f0f862efe5f17f4274aa481d
Ömer <okozluca.1@gmail.com>
2024-11-09 23:25:08 +0300

    Delete Odev9 directory

--- Odev9/Odev 9.1.sql
+++
@@ -1,4 +0,0 @@
-SELECT country.country , city.city FROM country
-INNER JOIN city ON country.country_id = city.city_id ;
---city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini 
---birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
--- Odev9/Odev 9.2.sql
+++
@@ -1,4 +0,0 @@
-SELECT payment.payment_id , customer.first_name , customer.last_name FROM customer
-INNER JOIN payment ON customer.customer_id=payment.payment_id;
---customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name 
---isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
